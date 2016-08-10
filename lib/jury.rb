f810c93eb76f72f8222a47f16ee4ff561d15c0a6
QilinGu <gump423@gmail.com>
2016-08-10 07:35:50 -0700

    Pass all the jury tests

---
+++ lib/jury.rb
@@ -0,0 +1,37 @@
+class Jury
+  attr_accessor :members
+  def initialize
+  	@members = []
+  end
+  def add_member(member)
+  	@members << member
+  end
+  def members
+  	@members
+  end
+  def cast_votes(finalists)
+  	votes = {}
+  	finalists.each do |finalist|
+  		votes[finalist] = 0
+  	end
+  	@members.each do |member|
+  	  vote = finalists.sample
+  	  puts "#{member} votes for #{vote}"
+  	  votes[vote] += 1
+  	end
+  	votes
+  end
+  def report_votes(votes)
+  	votes.each do |finalist, vote|
+  		puts "#{finalist}: #{vote}"
+  	end
+  end
+  def announce_winner(votes)
+  	votes.each do |finalist, vote|
+  		if vote >= 4
+  			puts "The winner is #{finalist.name}!"
+  			return finalist
+  		end
+  	end
+  end
+end
