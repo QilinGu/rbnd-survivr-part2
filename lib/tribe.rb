ff2f61047b584a3fcdbf9cafedcabe836452a684
Tony Gu <tgu@100597mb.msad.rentrak.com>
2016-08-10 13:53:17 -0700

    Pass all the tribe tests

---
+++ lib/tribe.rb
@@ -0,0 +1,16 @@
+class Tribe
+	attr_reader :name, :members
+	def initialize(options={})
+		@name = options[:name]
+		@members = options[:members]
+		puts "Tribe #{@name} successfully created!"
+	end
+	def to_s
+		@name
+	end
+	def tribal_council(options={})
+		eliminated = @members.reject {|member| member == options[:immune]}.sample
+		@members.delete(eliminated)
+		return eliminated
+	end
+end
